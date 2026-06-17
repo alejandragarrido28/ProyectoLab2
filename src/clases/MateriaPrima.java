27a8afa9f244d1cc602d1408c7d126a434940528
David Euceda <david_euceda@unitec.edu>
2026-06-16 21:52:08 -0600

    Materia Prima y Producto Electronico

---
+++ src/clases/MateriaPrima.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clases;
+
+import Enums.CategoriaProducto;
+
+/**
+ *
+ * @author euced
+ */
+public class MateriaPrima extends Producto {
+
+    private String unidadMedida;
+    private double concentracion;
+
+    public MateriaPrima(String unidadMedida, double concentracion, String codigo, String nombre, String proveedor, CategoriaProducto categoria, double precioUnitario, int stockActual, int stockMinimo) {
+        super(codigo, nombre, proveedor, categoria, precioUnitario, stockActual, stockMinimo);
+        this.unidadMedida = unidadMedida;
+        this.concentracion = concentracion;
+    }
+
+    
+    @Override
+    public double calcularValorStock() {
+        return super.getPrecioUnitario() * super.getStockActual();
+    }
+
+    @Override
+    public String generarAlerta() {
+        if (super.getStockActual() == 0) {
+            return String.format("CRITICO: Sin existencias - %s [%s] Contactar: %s", super.getNombre(), super.getCodigo(), super.getProveedor());
+        } else if (tieneStockCritico()) {
+            return String.format("ALERTA: Stock bajo (%d %s, min %d) - %s. Proveedor: %s",
+                    super.getStockActual(), unidadMedida, super.getStockMinimo(), super.getNombre(), super.getProveedor());
+        }
+        return "";
+
+    }
+
+    @Override
+    public String toRegistro() {
+        return super.toRegistro() + String.format("|%s|%.2f",
+                unidadMedida,
+                concentracion);
+    }
+}
---
+++ src/clases/ProductoElectrionico.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clases;
+
+import Enums.CategoriaProducto;
+
+/**
+ *
+ * @author euced
+ */
+public class ProductoElectrionico extends Producto {
+
+    private String numeroDeSerie;
+    private int garantiaMeses;
+    private int voltaje;
+
+    public ProductoElectrionico(String numeroDeSerie, int garantiaMeses, int voltaje, String codigo, String nombre, String proveedor, CategoriaProducto categoria, double precioUnitario, int stockActual, int stockMinimo) {
+        super(codigo, nombre, proveedor, categoria, precioUnitario, stockActual, stockMinimo);
+        this.numeroDeSerie = numeroDeSerie;
+        this.garantiaMeses = garantiaMeses;
+        this.voltaje = voltaje;
+    }
+
+    @Override
+    public double calcularValorStock() {
+        return super.getPrecioUnitario() * super.getStockActual() * (1 + garantiaMeses * 0.01);
+    }
+
+    @Override
+    public String generarAlerta() {
+        if (super.getStockActual() == 0) {
+            return String.format("CRITICO: Sin existencias - %s [%s] Serie: %s", super.getNombre(), super.getCodigo(), numeroDeSerie);
+        } else if (tieneStockCritico()) {
+            return String.format("ALERTA: Stock bajo (%d/%d min) - %s [%s]", super.getStockActual(), super.getStockMinimo(), super.getNombre(), super.getCodigo());
+        }
+        return "";
+    }
+
+}
